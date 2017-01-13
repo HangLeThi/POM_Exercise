b98e38998641618be24cabdbc6bde469e5cbbce5
HangLeThi <hanglt10@gmail.com>
2017-01-13 13:49:57 +0700

    Delete DefinitionTestSuite.java

--- tailored.test.adroind/src/test/java/demo/tailored/auto/DefinitionTestSuite.java
+++
@@ -1,9 +0,0 @@
-package demo.tailored.auto;
-
-import cucumber.api.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
-public class DefinitionTestSuite {}
